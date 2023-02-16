a9e2e747783130fc097b4ae5742d46181fa3590d
colorfultrees <colorfultrees@users.noreply.github.com>
2023-02-16 18:14:54 +0100

    class game added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/classes/game_class';
+
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
   cardStack = [0, 1, 2, 3];
   isCardPicked = false;
+  game: Game;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 
   pickCard() {
     this.isCardPicked = true;
---
+++ src/classes/game_class.ts
@@ -0,0 +1,19 @@
+import { shuffleArray } from "src/functions/aux_functions";
+
+export class Game {
+    private CARD_TYPES = ['clubs', 'diamonds', 'hearts', 'spade'];
+    public players: string[] = [];
+    public cardStack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() {
+        this.CARD_TYPES.forEach(type => {
+            for (let i = 0; i < 13; i++) {
+                this.cardStack.push(`${type}_${i}`);
+            }
+        })
+
+        shuffleArray(this.cardStack);
+    }
+}
